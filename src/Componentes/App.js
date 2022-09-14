84cf12f8ed70ed88395e3df276dfbc4d91c5c6c5
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-09-14 09:38:41 -0300

    componenetes criados

--- src/Componentes/App.js
+++ src/Componentes/App.js
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <div>
-            <h1>OLAA</h1>
+            <h1>OLAAA</h1>
         </div>
     )
-}
+}   
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom"
-import App from "./app"
+import App from "./Componentes/App"
 
 
 ReactDOM.render(<App/>, document.querySelector(".root"))
