2d462b99b0c59ba6018507c3277f3762d90be366
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-09-16 23:07:43 -0300

    array de posts e stories completo e renderizado

--- src/Componentes/Posts.js
+++ src/Componentes/Posts.js
@@ -1,79 +1,67 @@
+import React from "react"
+
 export default function Posts() {
-    return (
-        <>
-            <div class="posts">
-                <div class="post">
-                    <div class="topo">
-                        <div class="usuario">
-                            <img src="assets/img/meowed.svg" />
-                            meowed
-                        </div>
-                        <div class="acoes">
-                            <ion-icon name="ellipsis-horizontal"></ion-icon>
-                        </div>
+    const array = [
+        {
+            src: "assets/img/meowed.svg",
+            name: "meowed",
+            content: "assets/img/gato-telefone.svg",
+            likedSrc: "assets/img/respondeai.svg",
+            people: "respondeai",
+            number: "101.523",
+            liked: "Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>"
+        },
+        {
+            src: "assets/img/barked.svg",
+            name: "barked",
+            content: "assets/img/dog.svg",
+            likedSrc: "assets/img/adorable_animals.svg",
+            people: "adorable_animals",
+            number: "99.159",
+            liked: "Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>"
+        }
+    ]
+
+    const post = array.map((props) =>
+        <div class="post">
+            <div class="topo">
+                <div class="usuario">
+                    <img src={props.src} />
+                    {props.name}
+                </div>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+
+            <div class="conteudo">
+                <img src={props.content} />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
-
-                    <div class="conteudo">
-                        <img src="assets/img/gato-telefone.svg" />
-                    </div>
-
-                    <div class="fundo">
-                        <div class="acoes">
-                            <div>
-                                <ion-icon name="heart-outline"></ion-icon>
-                                <ion-icon name="chatbubble-outline"></ion-icon>
-                                <ion-icon name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div>
-                                <ion-icon name="bookmark-outline"></ion-icon>
-                            </div>
-                        </div>
-
-                        <div class="curtidas">
-                            <img src="assets/img/respondeai.svg" />
-                            <div class="texto">
-                                Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                            </div>
-                        </div>
+                    <div>
+                        <ion-icon name="bookmark-outline"></ion-icon>
                     </div>
                 </div>
 
-                <div class="post">
-                    <div class="topo">
-                        <div class="usuario">
-                            <img src="assets/img/barked.svg" />
-                            barked
-                        </div>
-                        <div class="acoes">
-                            <ion-icon name="ellipsis-horizontal"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="conteudo">
-                        <img src="assets/img/dog.svg" />
-                    </div>
-
-                    <div class="fundo">
-                        <div class="acoes">
-                            <div>
-                                <ion-icon name="heart-outline"></ion-icon>
-                                <ion-icon name="chatbubble-outline"></ion-icon>
-                                <ion-icon name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div>
-                                <ion-icon name="bookmark-outline"></ion-icon>
-                            </div>
-                        </div>
-
-                        <div class="curtidas">
-                            <img src="assets/img/adorable_animals.svg" />
-                            <div class="texto">
-                                Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                            </div>
-                        </div>
+                <div class="curtidas">
+                    <img src={props.likedSrc} />
+                    <div class="texto">
+                    Curtido por <strong>{props.people}</strong> e <strong>outras {props.number} pessoas</strong>
                     </div>
                 </div>
             </div>
+        </div>)
+
+    return (
+        <>
+{post}
         </>
     )
 }
--- src/Componentes/Stories.js
+++ src/Componentes/Stories.js
@@ -1,79 +1,56 @@
 export default function Stories() {
+
+    const stories = [
+        {
+            src: "assets/img/9gag.svg",
+            name: "9gag"
+        },
+        {
+            src: "assets/img/meowed.svg",
+            name: "meowed"
+        },
+        {
+            src: "assets/img/barked.svg",
+            name: "barked"
+        },
+        {
+            src: "assets/img/nathanwpylestrangeplanet.svg",
+            name: "nathanwpylestrangeplanet"
+        },
+        {
+            src: "assets/img/wawawicomics.svg",
+            name: "wawawicomics"
+        },
+        {
+            src: "assets/img/respondeai.svg",
+            name: "respondeai"
+        },
+        {
+            src: "assets/img/filomoderna.svg",
+            name: "filomoderna"
+        },
+        {
+            src: "assets/img/memeriagourmet.svg",
+            name: "memeriagourmet"
+        }
+    ]
+
+    const real = stories.map((st) =>
+        <div class="story">
+            <div class="imagem">
+                <img src={st.src} alt={st.name} />
+            </div>
+            <div class="usuario">
+                {st.name}
+            </div>
+        </div>)
+
     return (
         <>
             <div class="stories">
-                <div class="story">
-                    <div class="imagem">
-                        <img src="assets/img/9gag.svg" />
-                    </div>
-                    <div class="usuario">
-                        9gag
-                    </div>
-                </div>
 
-                <div class="story">
-                    <div class="imagem">
-                        <img src="assets/img/meowed.svg" />
-                    </div>
-                    <div class="usuario">
-                        meowed
-                    </div>
-                </div>
-
-                <div class="story">
-                    <div class="imagem">
-                        <img src="assets/img/barked.svg" />
-                    </div>
-                    <div class="usuario">
-                        barked
-                    </div>
-                </div>
-
-                <div class="story">
-                    <div class="imagem">
-                        <img src="assets/img/nathanwpylestrangeplanet.svg" />
-                    </div>
-                    <div class="usuario">
-                        nathanwpylestrangeplanet
-                    </div>
-                </div>
-
-                <div class="story">
-                    <div class="imagem">
-                        <img src="assets/img/wawawicomics.svg" />
-                    </div>
-                    <div class="usuario">
-                        wawawicomics
-                    </div>
-                </div>
-
-                <div class="story">
-                    <div class="imagem">
-                        <img src="assets/img/respondeai.svg" />
-                    </div>
-                    <div class="usuario">
-                        respondeai
-                    </div>
-                </div>
-
-                <div class="story">
-                    <div class="imagem">
-                        <img src="assets/img/filomoderna.svg" />
-                    </div>
-                    <div class="usuario">
-                        filomoderna
-                    </div>
-                </div>
-
-                <div class="story">
-                    <div class="imagem">
-                        <img src="assets/img/memeriagourmet.svg" />
-                    </div>
-                    <div class="usuario">
-                        memeriagourmet
-                    </div>
-                </div>
-
+                {real}
+                
                 <div class="setinha">
                     <ion-icon name="chevron-forward-circle"></ion-icon>
                 </div>
